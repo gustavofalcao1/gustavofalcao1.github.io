2de18dae73de7715c7d506601abc163a8009a546
gustavofalcao1 <gustavo.falcao01@gmail.com>
2023-03-06 20:29:42 +0000

    downgrad loading time

--- components/js/loading.js
+++ components/js/loading.js
@@ -3,9 +3,9 @@ var lang = navigator.language;
 if (lang === 'pt-BR' || lang === 'pt-PT'){
   var timer = setTimeout(function() {
     window.location='/pt'
-  }, 1000);
+  }, 1);
 } else {
   var timer = setTimeout(function() {
     window.location='/en'
-  }, 1000);
+  }, 1);
 }
